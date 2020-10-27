cca7d41db6bd875eeb83a44658702b155cb1b9bc
abel-mesfin <73157783+abel-mesfin@users.noreply.github.com>
2020-10-27 00:00:51 -0700

    Update myfuncs.php

--- myfuncs.php
+++ myfuncs.php
@@ -9,13 +9,20 @@ The modules purpose is to create a html form that shows the user the required fi
 <?php
 
 function dbConnect() {
-//Mysql connection info
-$servername = "127.0.0.1:54939";
-$username = "azure";
-$password = "6#vWHD_$";
-$dbname = "activity1";
-	
-$conn = new mysqli($servername, $username, $password, $dbname);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:abelsblogdb.database.windows.net,1433; Database = activity1", "abelmesfin", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "abelmesfin", "pwd" => "{your_password_here}", "Database" => "activity1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:abelsblogdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 return $conn;
 
