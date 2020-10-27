2311375657c8b74429f8d974c661b83e84a19787
abel-mesfin <73157783+abel-mesfin@users.noreply.github.com>
2020-10-27 03:02:25 -0700

    Update loginResponse.php

--- loginResponse.php
+++ loginResponse.php
@@ -11,6 +11,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once('myfuncs.php');
+$username = $_POST['username'];
 
 ?>
 <?php if(!empty(getUserId())): ?>
